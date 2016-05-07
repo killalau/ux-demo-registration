b97803e9494a8246f9f8675426bccaf18ca35b34
Franky Lau <killalau1988@hotmail.com>
2016-05-07 23:10:27 +0800

    Remove source code not related to github page

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="react/react.d.ts" />
-/// <reference path="react/react-dom.d.ts" />
-/// <reference path="material-ui/material-ui.d.ts" />
-/// <reference path="node/node.d.ts" />
